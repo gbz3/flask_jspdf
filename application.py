59b0858763c5b2d88fb17a0fbf7d667abc543ce7
gbz3 <gebizo3@gmail.com>
2020-02-15 00:51:25 +0000

    add templates

--- application.py
+++ application.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return "hello world..."
+
+@app.route('/pdf')
+def pdf():
+  return render_template('pdf.html')
 
